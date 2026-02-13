3e57c73a5802ec5e93e9996e0f6beb8ba70c05d9
Mohamed Emam <126331291+mohamed-em2m@users.noreply.github.com>
2026-02-13 00:20:40 +0000

    clean code

--- http/get_request.py
+++ http/get_request.py
@@ -1,20 +1,22 @@
 import socket
 import ssl
 
-host="example.com"
-port=443
+host = "example.com"
+port = 443
 
-sock=socket.create_connection((host,port))
+sock = socket.create_connection((host, port))
 
-context=ssl.create_default_context()
-safe_sock=context.wrap_socket(sock,server_hostname=host)
+context = ssl.create_default_context()
+safe_sock = context.wrap_socket(sock, server_hostname=host)
 
-request=("GET / HTTP/1.1\r\n"
-         f"Host: {host}\r\n"
-         "User-Agent: low-level-client/1.0\r\n"
-         "Accept: */*\r\n"
-         "Connection: close\r\n"
-         "\r\n")
+request = (
+    "GET / HTTP/1.1\r\n"
+    f"Host: {host}\r\n"
+    "User-Agent: low-level-client/1.0\r\n"
+    "Accept: */*\r\n"
+    "Connection: close\r\n"
+    "\r\n"
+)
 
 safe_sock.sendall(request.encode())
 
@@ -22,7 +24,7 @@ request = b""
 
 while True:
     data = safe_sock.recv(4096)
-    
+
     if not data:
         break
 
@@ -30,23 +32,25 @@ while True:
 
 safe_sock.close()
 
+
 def decode_chunked(body):
     decode = ""
     i = 0
     while True:
-        j = body.find("\r\n",i)
+        j = body.find("\r\n", i)
         chunk_size_hex = body[i:j].strip()
-        chunk_size = int(chunk_size_hex,16)
+        chunk_size = int(chunk_size_hex, 16)
         if chunk_size == 0:
             break
-        i = j+2
-        decode+ = body[i:i+chunk_size]
-        i+ = chunk_size+2
+        i = j + 2
+        decode += body[i : i + chunk_size]
+        i += chunk_size + 2
     return decode
 
-headers,body = request.decode(errors='ignore').split("\r\n\r\n",1)
+
+headers, body = request.decode(errors="ignore").split("\r\n\r\n", 1)
 
 if "transfer-encoding: chunked" in headers.lower():
     body = decode_chunked(body)
 
-print("body",body)
+print("body", body)
--- http/llm_request.py
+++ http/llm_request.py
@@ -2,8 +2,8 @@ import ssl
 import json
 import socket
 
-host="generativelanguage.googleapis.com"
-port=443
+host = "generativelanguage.googleapis.com"
+port = 443
 API_KEY = "Your Token"
 
 body = """{
@@ -16,11 +16,11 @@ body = """{
     ]
   }""".encode()
 
-sock=socket.socket()
+sock = socket.socket()
 
 sock.connect((host, port))
 
-requester= ssl.create_default_context().wrap_socket(sock,server_hostname=host)
+requester = ssl.create_default_context().wrap_socket(sock, server_hostname=host)
 
 request = (
     "POST /v1beta/openai/chat/completions HTTP/1.1\r\n"
@@ -29,32 +29,31 @@ request = (
     "Content-Type: application/json\r\n"
     f"Content-Length: {len(body)}\r\n"
     "Connection: close\r\n"
-    "\r\n"  
+    "\r\n"
 )
 
-requester.sendall(request.encode()+body)
+requester.sendall(request.encode() + body)
 
-response=b""
+response = b""
 
-while True :
-  
-    chunk=requester.recv(4096)
+while True:
+    chunk = requester.recv(4096)
     if not chunk:
         break
-    response+=chunk
+    response += chunk
 
-header,body = response.split(b"\r\n\r\n",1)
-readed_body=b""
-start_index=0
-split_char_length=2
-while start_index<len(body):
-  split_index = body.find(b"\r\n",start_index)
-  chunk_length = int( body[start_index:split_index].strip() , 16)
-  if not chunk_length:
-    break
-  start_index = split_index + split_char_length
-  end_data= start_index + chunk_length
-  readed_body+= body[start_index:end_data]
-  start_index+= split_char_length + chunk_length
+header, body = response.split(b"\r\n\r\n", 1)
+readed_body = b""
+start_index = 0
+split_char_length = 2
+while start_index < len(body):
+    split_index = body.find(b"\r\n", start_index)
+    chunk_length = int(body[start_index:split_index].strip(), 16)
+    if not chunk_length:
+        break
+    start_index = split_index + split_char_length
+    end_data = start_index + chunk_length
+    readed_body += body[start_index:end_data]
+    start_index += split_char_length + chunk_length
 
-print("body:",json.loads(readed_body.decode('utf-8')))
+print("body:", json.loads(readed_body.decode("utf-8")))
